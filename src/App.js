7e206b7a2f56d756a59b31248ecb8d56eb9f417f
Nick O <nicholas.oetinger@gmail.com>
2022-02-09 20:48:58 -0500

    Added global styles, Header and FeedbackItem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
 function App() {
   return (
-    <div className="container">
-      <h1>My App</h1>
-    </div>
-  )
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
